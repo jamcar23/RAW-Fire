98db8ab3c1bb451d0b02763a72b455236cbde52a
jamcar23 <jamcar23@gmail.com>
2016-09-19 16:55:48 -0400

    Copyright

--- dependencies_osx.sh
+++ dependencies_osx.sh
@@ -1,20 +1,24 @@
 #! /bin/bash
 
+# Copyright (c) 2016 James Carroll
+# This file is part of RAW Fire.
+#
+# RAW Fire is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# RAW Fire is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with RAW Fire.  If not, see <http://www.gnu.org/licenses/>.
+
 # Download submodules
 git submodule update --init --recursive
 
 # Install OSX dependencies via brew
 brew tap homebrew/versions
 brew install cmake freeimage fftw boost glew glfw3
-
-root_dir="./"
-build="/build/"
-
-# Arrayfire
-af_path="./arrayfire-rust/arrayfire"
-mkdir $af_path$build
-cd $af_path$build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_CUDA=OFF -DBUILD_OPENCL=ON
-make -j8
-sudo make install
-cd $root_dir
--- dependencies_ubuntu.sh
+++ dependencies_ubuntu.sh
@@ -1,8 +1,26 @@
 #! /bin/bash
 
+# Copyright (c) 2016 James Carroll
+# This file is part of RAW Fire.
+#
+# RAW Fire is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# RAW Fire is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with RAW Fire.  If not, see <http://www.gnu.org/licenses/>.
+
 # Downloads dependencies on Ubunutu 16.04
 # Other Ubuntu versions will likely work but things may have been renamed.
 # Untested (for now)
+
+git submodule update --init --recursive
 
 sudo apt-get install -y build-essential git cmake libfreeimage-dev cmake-curses-gui \
   libglew-dev libatlas3-base libatlas-dev libfftw3-dev liblapacke-dev \
